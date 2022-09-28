00070a2ef32fbbdda5a79e808286c443e8abc29c
AnnaLopushyk <anna.lopushyk@gmail.com>
2022-09-29 01:01:40 +0300

    !

---
+++ src/components/characters/Character.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Character(props) {
+    const {character} = props;
+
+    return (
+        <div>
+            <h3>{character.id} -- {character.name}</h3>
+            <p>{character.status}</p>
+        </div>
+    );
+}
+
+export default Character;
