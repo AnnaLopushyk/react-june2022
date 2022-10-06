419cf2bb2e95ea760ab9b98a009dab9adfc0a3d8
AnnaLopushyk <anna.lopushyk@gmail.com>
2022-10-06 23:07:04 +0300

    hw3-lesson!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-import Simpsons from "./components/Simpsons";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
 
 function App() {
+
+    const state = useSelector(state => state.userReduser);
+    console.log(state)
+    const dispatch = useDispatch();
+
+
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => {
+                dispatch( {type:'LOAD_USERS', payload:value})
+
+            })
+    },[])
+    // dispatch( {type:'CHOOSE_USER', payload:1})
+    // dispatch( {type:'LOAD_POSTS', payload: 'posts'});
+
     return (
         <div className="App">
-            <Simpsons/>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,80 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {combineReducers, createStore} from "redux";
+
+
+const userReduser = (state = {users: [], user: null}, action) => {
+    switch (action.type) {
+        case "LOAD_USERS":
+            return {
+                ...state,
+                users: action.payload
+            };
+        case "CHOOSE_USER":
+            return
+            let id = action.payload;
+            let user = state.users.find(value => value.id === id);
+            return {...state, user: user}
+
+        default:
+            return state;
+    }
+
+}
+
+
+const postReduser = (state = {posts: [], post: null}, action) => {
+    switch (action.type) {
+        case "LOAD_POSTS":
+            return {
+                ...state,
+                posts: action.payload
+            };
+        case "CHOOSE_POST":
+            return
+            let id = action.payload;
+            let post = state.posts.find(value => value.id === id);
+            return {...state, post: post}
+        default:
+            return state;
+    }
+
+}
+
+
+const commentReduser = (state = {comments: [], comment: null}, action) => {
+    switch (action.type) {
+        case "LOAD_COMMENTS":
+            return {
+                ...state,
+                comment: action.payload
+            };
+        case "CHOOSE_COMMENT":
+            return
+            let id = action.payload;
+            let comment = state.comments.find(value => value.id === id);
+            return {...state, comment: comment}
+        default:
+            return state;
+    }
+
+}
+
+const reducer = combineReducers({
+    userReduser: userReduser,
+    postReduser : postReduser,
+    commentReduser: commentReduser
+});
+
+let store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
